ac5e6f69fe79b504dc4fa001b8fefa2d88baf32e
Isaac Augusto <isaac.augusto@hotmail.com>
2020-12-01 13:23:10 -0300

    Finalmente carai

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import math 
 import os.path
 from os import path
 import time
 import sys
 import random
+import collections
+from itertools import permutations
 
 #Função que lê as arestas criadas pelo gerador de arestas
 def _ReadFromFile_():
@@ -33,36 +36,54 @@ def _AddResults_(node, color):
     return "Torre " +str(node) + " = " + color + "\n"
 
 def _GenerateResultsFile_(rlist):
-    print (rlist)
     file = open("Results.txt", "w")
     file.write(rlist)
     file.close()
 
+def _GetColorMap_(cList):
+    orderList = []
+    for k in range(len(list(G.nodes))):
+        for i in cList.items():
+            if i[0] == int(list(G.nodes)[k]):
+                orderList.append(i[1])
+    return orderList
+
 G = nx.Graph() #Crio o grafo
-ResultList = ""
 Arestas = _ReadFromFile_() #Leio as arestas
 _AddEdgeInGraph_(Arestas) #Adiciono as arestas
 _SetAllNodeColorsToGray_(G)#Pinta todas as arestas de cinza
-baseColorMap = [] #Cores disponiveis
-colorMap = [] #Colormap para fazer a coloração do grafo
-start_time = time.time() #Calcular tempo de execução
-for nd in G.nodes: #Loop por todos os nodes
-    colorChoices = baseColorMap[:] #Clono as cores disponíveis
-    for vizinho in G[nd]: #Loop por todos os vizinhos do vertice em questão
-        if (G._node[vizinho]["color"] in colorChoices): #Se a cor do vizinho atual ja esta no vetor (já foi colorido)
-            colorChoices.remove(G._node[vizinho]["color"]) #Remove aquela cor pois não esta mais disponível
-    if len(colorChoices) > 0: #Se não tiver removido todas as cores
-        G._node[nd]["color"] = colorChoices[0] #Adiciona a primeira cor disponível restante no vetor
-    else:
-        myColor = "#{:06x}".format(random.randint(0, 0xFFFFFF))
-        while (myColor in baseColorMap):
-            myColor = "#{:06x}".format(random.randint(0, 0xFFFFFF))
-        baseColorMap.append(myColor)
-        G._node[nd]["color"] = myColor
-    colorMap.append(G._node[nd]["color"]) #Adiciona a cor ao mapa de cores
-    ResultList += _AddResults_(nd, G._node[nd]["color"])
-print("--- %f segundos ---" % (time.time() - start_time))#Calcular tempo de execução
+nodesList = list(G.nodes)
+perm = permutations(nodesList)
+BestResultList = ""
+BestColorNumer = math.inf
+BestColorMap = []
 
-_GenerateResultsFile_(ResultList)
-nx.draw(G, node_color = colorMap,with_labels=True) #Desenha o grafo
+start_time = time.time() #Calcular tempo de execução
+for p in list(perm):
+    ResultList = ""
+    colorMap = {} #Colormap para fazer a coloração do grafo
+    baseColorMap = [] #Cores disponiveis
+    for nd in p: #Loop por todos os nodes
+        colorChoices = baseColorMap[:] #Clono as cores disponíveis
+        for vizinho in G[nd]: #Loop por todos os vizinhos do vertice em questão
+            if (G._node[vizinho]["color"] in colorChoices): #Se a cor do vizinho atual ja esta no vetor (já foi colorido)
+                colorChoices.remove(G._node[vizinho]["color"]) #Remove aquela cor pois não esta mais disponível
+        if len(colorChoices) > 0: #Se não tiver removido todas as cores
+            G._node[nd]["color"] = colorChoices[0] #Adiciona a primeira cor disponível restante no vetor
+        else:
+            myColor = "#{:06x}".format(random.randint(0, 0xFFFFFF))
+            baseColorMap.append(myColor)
+            G._node[nd]["color"] = myColor
+        colorMap[int(nd)] = G._node[nd]["color"] #Adiciona a cor ao mapa de cores
+        ResultList += _AddResults_(nd, G._node[nd]["color"])
+    if (len(baseColorMap) < BestColorNumer):
+        orderColorMap = _GetColorMap_(colorMap)
+        BestResultList = ResultList
+        BestColorMap = orderColorMap
+        BestColorNumer = len(baseColorMap)
+print("--- %f segundos ---" % (time.time() - start_time))#Calcular tempo de execução
+print("Numero de cores usadas: " + str(BestColorNumer))
+
+_GenerateResultsFile_(BestResultList)
+nx.draw(G, node_color = BestColorMap,with_labels=True) #Desenha o grafo
 plt.show() #Mostra em uma janela
